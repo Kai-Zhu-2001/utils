4938f1a790865060fd305ad29994752f95fa7162
Kai Zhu <22319143@zju.edu.cn>
2026-07-12 18:36:00 +0200

    Update plumed.sh for new paths and configuration options

--- configure/plumed.sh
+++ configure/plumed.sh
@@ -1,33 +1,26 @@
-# PyTorch path
-export TORCH_DIR=/work/kzhu/anaconda3/envs/mlcolvar-gnn/lib/python3.11/site-packages/torch
+#!/usr/bin/env bash
 
-# Library paths
-export LD_LIBRARY_PATH=$TORCH_DIR/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/work/kzhu/opt/plumed2.9.4-gnn/lib:$LD_LIBRARY_PATH
+SOURCE_DIR=/leonardo/home/userexternal/kzhu0000/opt/plumed-2.10.0
+INSTALL_DIR=/leonardo/home/userexternal/kzhu0000/opt/plumed2.10.0
 
-# Use C++17 for libtorch
-sed -i "s/c++11/c++17/g" conf*
-sed -i "s/c++14/c++17/g" conf*
-sed -i "s/c++11/c++17/g" Makefile*
-sed -i "s/c++14/c++17/g" Makefile*
+cd "$SOURCE_DIR"
 
-# Torch / NCCL linking
-export LDFLAGS="
--L$TORCH_DIR/lib -Wl,-rpath,$TORCH_DIR/lib
--L$EBROOTNCCL/lib -Wl,-rpath,$EBROOTNCCL/lib
--lc10 -ltorch_cpu -lc10_cuda -ltorch_cuda -lnccl
-"
+CPPFLAGS="$(python src/metatomic/flags-from-python.py --cppflags)"
+LDFLAGS="$(python src/metatomic/flags-from-python.py --ldflags)"
 
-# Configure PLUMED
 ./configure \
-CXX=mpicxx \
-CXXFLAGS="-O3 -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=1" \
-CPPFLAGS="-I$TORCH_DIR/include -I$TORCH_DIR/include/torch/csrc/api/include" \
-LDFLAGS="-L$TORCH_DIR/lib -Wl,-rpath,$TORCH_DIR/lib" \
---prefix=/work/kzhu/opt/plumed2.9.4-gnn \
---enable-mpi \
---enable-openmp \
---enable-libtorch \
---enable-modules=all \
+  CXX=mpicxx \
+  CXXFLAGS="-O3 -std=c++17" \
+  CPPFLAGS="$CPPFLAGS" \
+  LDFLAGS="$LDFLAGS" \
+  --prefix="$INSTALL_DIR" \
+  --enable-mpi \
+  --enable-openmp \
+  --enable-libtorch \
+  --enable-libmetatomic \
+  --enable-modules=all \
+  --disable-external-blas \
+  --disable-external-lapack
 
-make -j 8 install
+make -j 2
+make install
